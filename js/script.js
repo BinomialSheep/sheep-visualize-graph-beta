945bbd5be3802de9ac23d6a70a2bd547c3ab7ba7
BinomialSheep <jose_jinrou@yahoo.co.jp>
2024-01-05 22:19:59 +0900

    ファイル切り分け

--- js/script.js
+++ js/script.js
@@ -1,18 +1,3 @@
-const colorPallet = new Array('#b2cbe4', '#e3b1ca', '#e3cab1', '#b1e3ca');
-
-const getNextColor = (nowColor) => {
-  for (let i = 0; i < colorPallet.length; i++) {
-    if (nowColor == colorPallet[i]) return colorPallet[++i % colorPallet.length];
-  }
-  return colorPallet[0];
-};
-const getPrevColor = (nowColor) => {
-  for (let i = 0; i < colorPallet.length; i++) {
-    if (nowColor == colorPallet[i]) return colorPallet[(--i + colorPallet.length) % colorPallet.length];
-  }
-  return colorPallet[0];
-};
-
 const addEvents = (network, data) => {
   // 頂点を左クリックで色の変更
   network.on('click', (params) => {
